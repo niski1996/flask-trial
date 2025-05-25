ee1334014a72958fcf6676923779fff7ca86f0a5
niski1996 <niski1996@users.noreply.github.com>
2025-05-25 17:47:02 +0000

    ✅ Ready to clone and code.

--- flask_trial/ext/commands.py
+++ flask_trial/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from flask_trial.ext.auth import create_user
+from flask_trial.ext.database import db
+from flask_trial.models import Product
 
 
 def create_db():
